946ce9d445686eb49d9d161a0b07113b33bb8936
Tony Bowden <tony@tmtm.com>
2017-01-14 09:29:47 +0000

    rewrite using Scraped

--- scraper.rb
+++ scraper.rb
@@ -9,66 +9,104 @@ require 'scraperwiki'
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
+class ListPage < Scraped::HTML
+  field :parties do
+    composition.css('h3').map do |h3|
+      fragment h3 => PartySection
+    end
+  end
+
+  private
+
+  def composition
+    noko_between(noko, 'Samenstelling_van_de_kamer_sinds_12_september_2012', 'Bijzonderheden')
+  end
+
+  def noko_between(noko, from_id, to_id)
+    section_start = noko.css('#' + from_id)
+    raise "Can't find start section #{from_id}" if section_start.empty?
+
+    section_end = noko.css('#' + to_id)
+    raise "Can't find end section #{to_id}" if section_end.empty?
+
+    section_start.xpath('.//preceding::*').remove
+    section_end.xpath('.//following::*').remove
+    noko
+  end
 end
 
-def noko_between(noko, from_id, to_id)
-  section_start = noko.css('#' + from_id)
-  raise "Can't find start section #{from_id}" if section_start.empty?
+class PartySection < Scraped::HTML
+  field :party do
+    party_info.children.first.text.split('(').first.tidy
+  end
 
-  section_end = noko.css('#' + to_id)
-  raise "Can't find end section #{to_id}" if section_end.empty?
+  field :party_wikiname do
+    party_info.xpath('.//a[not(@class="new")]/@title').text
+  end
 
-  section_start.xpath('.//preceding::*').remove
-  section_end.xpath('.//following::*').remove
-  noko
+  field :members do
+    following_lists.flat_map { |e| e.css('li') }.map do |li|
+      fragment li => MemberSection
+    end
+  end
+
+  private
+
+  def party_info
+    noko.children.first
+  end
+
+  def following_lists
+    noko.xpath('following::h3 | following::ul').slice_before { |e| e.name == 'h3' }.first
+  end
 end
 
-def reverse_date(str)
-  str.split('-').reverse.map { |d| sprintf '%02d', d }.join('-')
+class MemberSection < Scraped::HTML
+  field :name do
+    noko.children.first.children.first.text
+  end
+
+  field :wikiname do
+    noko.xpath('.//a[not(@class="new")]/@title').text
+  end
+
+  field :memberships do
+    dates = [{ type: 'start', date: '' }]
+    noko.text.scan(/\(([^\)]+)\)/).flatten.each do |bracketed|
+      if sd = bracketed[/↑ (.*)/, 1]
+        dates << { type: 'end', date: reverse_date(sd) }
+      elsif sd = bracketed[/↓ (.*)/, 1]
+        dates << { type: 'start', date: reverse_date(sd) }
+      end
+    end
+
+    dates << { type: 'end', date: '' } unless dates[-1] && dates[-1][:type] == 'end'
+    dates.shift if dates[1] && dates[1][:type] == 'start'
+    dates.each_slice(2).map do |from, to|
+      { start_date: from[:date], end_date: to[:date] }
+    end
+  end
+
+  private
+
+  def reverse_date(str)
+    str.split('-').reverse.map { |d| '%02d' % d }.join('-')
+  end
 end
 
-def scrape_term(url)
-  noko = noko_for(url)
-  list = noko_between(noko, 'Samenstelling_van_de_kamer_sinds_12_september_2012', 'Bijzonderheden')
-  list.css('h3').each do |h3|
-    party_info = h3.children.first
-    party = party_info.children.first.text.split('(').first.tidy
-    party_wikiname = party_info.xpath('.//a[not(@class="new")]/@title').text
-
-    people_lists = h3.xpath('following::h3 | following::ul').slice_before { |e| e.name == 'h3' }.first
-    people_lists.each do |people_list|
-      people_list.css('li').each do |person|
-        dates = [{ type: 'start', date: '' }]
-
-        person.text.scan(/\(([^\)]+)\)/).flatten.each do |bracketed|
-          if sd = bracketed[/↑ (.*)/, 1]
-            dates << { type: 'end', date: reverse_date(sd) }
-          elsif sd = bracketed[/↓ (.*)/, 1]
-            dates << { type: 'start', date: reverse_date(sd) }
-          end
-        end
-
-        dates << { type: 'end', date: '' } unless dates[-1] && dates[-1][:type] == 'end'
-        dates.shift if dates[1] && dates[1][:type] == 'start'
-
-        dates.each_slice(2).each do |from, to|
-          data = {
-            name:           person.children.first.children.first.text,
-            wikiname:       person.xpath('.//a[not(@class="new")]/@title').text,
-            party:          party,
-            party_wikiname: party_wikiname,
-            term:           2012,
-            start_date:     from[:date],
-            end_date:       to[:date],
-          }
-          ScraperWiki.save_sqlite(%i(name wikiname party start_date term), data)
-        end
+def term_data(url)
+  page = ListPage.new(response: Scraped::Request.new(url: url).response)
+  page.parties.map(&:to_h).flat_map do |party|
+    party.delete(:members).map(&:to_h).flat_map do |member|
+      member.delete(:memberships).map do |membership|
+        member.merge(party).merge(term: 2012).merge(membership)
       end
     end
   end
 end
 
+data = term_data('https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden')
+# puts data
+
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_term('https://nl.wikipedia.org/wiki/Samenstelling_Tweede_Kamer_2012-heden')
+ScraperWiki.save_sqlite(%i(name wikiname party start_date term), data)
